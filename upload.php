b497c9d12d2e7d2a4d11f90521e0e043f9133ace
yanzhifanghuoqiang <shiyuhong@hdu.edu.cn>
2019-11-20 10:56:21 +0800

    Create upload.php

---
+++ upload.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+$file = $_FILES['files'];
+$fileName = $file['name'];
+move_uploaded_file($file['tmp_name'],$fileNamme);
+
+echo "<img src='$fileName'>";
+?>
+</body>
+</html>
