24e3a193cd8436288a2b647c489fae3c705c1e29
yanzhifanghuoqiang <shiyuhong@hdu.edu.cn>
2019-11-20 10:26:19 +0800

    Create add.php

---
+++ add.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+if($_POST['a']&&$_POST['b']){
+    echo $_POST['a']+$_POST['b'];
+}else
+    echo '请输入参数';
+}
+ 
+?>
+ 
+</body> </html>
+ 
